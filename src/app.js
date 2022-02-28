fe0d70b131b38ac9fd146ad159070160b88bb2fb
joseph achonu <jachonu@axxess.com>
2022-02-28 09:50:42 +0100

    completed transaction

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import Web3 from "web3";
-import EthereumTransaction from "ethereumjs-tx";
-import dotenv from "dotenv";
+const Web3 = require("web3");
+const EthereumTransaction = require("ethereumjs-tx").Transaction;
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -8,45 +8,38 @@ const createEthereumTransaction = async () => {
   // -- Step 1: Set up the appropriate configuration
   let localGanacheUrl = "http://127.0.0.1:8545";
   let web3 = new Web3(localGanacheUrl);
-  web3.eth.getAccounts().then((accounts) => console.log(accounts));
+  let accounts = await web3.eth.getAccounts();
+  console.log("Account in local ganache chain:", accounts);
 
   // -- Step 2: Set the sending and receiving addresses for the transaction
-  let sendingAddress = "0x14898b8a6De59e959d057E9d294eA66d57eBd1F3";
-  let receivingAddress = "0xe40c6f8171e6B137391c69e04dc6b98bF1dc02dB";
+  let sendingAddress = accounts[0];
+  let receivingAddress = accounts[1];
 
   // -- Step 3: Check the balances of each address
-  web3.eth
-    .getBalance(sendingAddress)
-    .then((bal) => console.log("SendingAddress Bal:", bal));
-  web3.eth
-    .getBalance(receivingAddress)
-    .then((bal) => console.log("ReceivingAddress Bal:", bal));
+  console.log("SendingAddress Bal Before:", await web3.eth.getBalance(sendingAddress));
+  console.log("ReceivingAddress Bal Before:", await web3.eth.getBalance(receivingAddress));
 
   // -- Step 4: Set up the transaction using the transaction variables
   let rawTxn = {
-    nonce: 0,
-    gasPrice: 20000000,
-    gasLimit: 40000,
-    value: 1,
-    data: "",
+    nonce: "0x00",
+    gasPrice: "0x09184e72a000",
+    gasLimit: "0xC350",
+    to: receivingAddress,
+    value: "0x14",
+    data: "0x7f7465737432000000000000000000000000000000000000000000000000000000600057",
   };
 
-  // -- Step 5: View the raw transaction rawTransaction
-  console.log("Raw Transaction:", rawTxn);
-
-  // -- Step 6: Check the new account balances (they should be the same)
-  web3.eth.getBalance(sendingAddress).then(console.log);
-  web3.eth.getBalance(receivingAddress).then(console.log);
-
-  // Note: They haven't changed because they need to be signed...
-
-  // -- Step 7: Sign the transaction with the Hex value of the private key of the sender
-  let senderPrivateKey = process.env.SENDER_PRIVATE_KEY;
-  console.log("senderPrivateKey:", senderPrivateKey);
-  let senderPrivateKeyHex = new Buffer.from(senderPrivateKey, "hex");
-  console.log("senderPrivateKeyHex:", senderPrivateKeyHex);
-  let txn = new EthereumTransaction.Transaction(rawTxn);
+  // -- Step 6: Sign the transaction with the Hex value of the private key of the sender
+  let senderPrivateKey = process.env.PRI_KEY;
+  let senderPrivateKeyHex = Buffer.from(senderPrivateKey, "hex");
+  let txn = new EthereumTransaction(rawTxn);
   txn.sign(senderPrivateKeyHex);
+  var serializedTransaction = txn.serialize();
+  await web3.eth.sendSignedTransaction(serializedTransaction);
+
+  //View Account balances after transactions
+  console.log("SendingAddress Bal After:", await web3.eth.getBalance(sendingAddress));
+  console.log("ReceivingAddress Bal After:", await web3.eth.getBalance(receivingAddress));
 };
 
 createEthereumTransaction();
